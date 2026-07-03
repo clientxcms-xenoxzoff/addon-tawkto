bf36319b165049680c9d6674f0e56009548cabc7
XenoXzOFF <xenoxzperso@gmail.com>
2026-07-03 03:33:44 +0200

    Update settings.blade.php

--- addons/tawkto/views/admin/settings.blade.php
+++ addons/tawkto/views/admin/settings.blade.php
@@ -6,24 +6,21 @@
             <h4 class="font-semibold uppercase text-gray-600 dark:text-gray-400">
                 {{ __('tawkto::messages.admin.settings.title') }}
             </h4>
-        </div>
-        <form method="POST" action="{{ route('tawkto.settings') }}">
-            @csrf
-            @method('PUT')
-            <div class="grid grid-cols-1 gap-4 mb-4">
-                <div>
-                    @include('shared/input', [
-                        'name' => 'tawkto_chat_url',
-                        'label' => __('tawkto::messages.admin.settings.chat_url'),
-                        'value' => setting('tawkto_chat_url'),
-                        'placeholder' => 'https://tawk.to/chat/xxxxxxxxxxxx/xxxxxxxxx',
-                    ])
+            <form method="POST" action="{{ route('tawkto.settings') }}">
+                @csrf
+                @method('PUT')
+                <div class="grid grid-cols-1 gap-4">
+                    <div>
+                        @include('shared/input', [
+                            'name' => 'tawkto_chat_url',
+                            'label' => __('tawkto::messages.admin.settings.chat_url'),
+                            'value' => setting('tawkto_chat_url'),
+                            'placeholder' => 'https://tawk.to/chat/xxxxxxxxxxxx/xxxxxxxxx',
+                        ])
+                    </div>
                 </div>
-            </div>
-            <p class="text-sm text-gray-500 dark:text-gray-400 mt-2">
-                {{ __('tawkto::messages.admin.settings.chat_url_help') }}
-            </p>
-            <button type="submit" class="btn btn-primary">{{ __('global.save') }}</button>
-        </form>
+                <button type="submit" class="btn btn-primary">{{ __('global.save') }}</button>
+            </form>
+        </div>
     </div>
 @endsection
