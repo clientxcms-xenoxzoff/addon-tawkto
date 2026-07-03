c7fad1d40b30aff2937d9ce0a7a9dac61873b3ca
XenoXzOFF <xenoxzperso@gmail.com>
2026-07-03 04:34:53 +0200

    Move form below title, not beside it

--- addons/tawkto/views/admin/settings.blade.php
+++ addons/tawkto/views/admin/settings.blade.php
@@ -6,22 +6,22 @@
             <h4 class="font-semibold uppercase text-gray-600 dark:text-gray-400">
                 {{ __('tawkto::messages.admin.settings.title') }}
             </h4>
-            <form method="POST" action="{{ route('tawkto.settings') }}">
-                @csrf
-                @method('PUT')
-                <div class="grid grid-cols-1 gap-4">
-                    <div>
-                        @include('shared/input', [
-                            'name' => 'tawkto_chat_url',
-                            'label' => __('tawkto::messages.admin.settings.chat_url'),
-                            'value' => setting('tawkto_chat_url'),
-                            'placeholder' => 'https://tawk.to/chat/xxxxxxxxxxxx/xxxxxxxxx',
-                        ])
-                        <p class="text-sm text-gray-500 mt-1">{{ __('tawkto::messages.admin.settings.chat_url_help') }}</p>
-                    </div>
-                </div>
-                <button type="submit" class="btn btn-primary">{{ __('global.save') }}</button>
-            </form>
         </div>
+        <form method="POST" action="{{ route('tawkto.settings') }}">
+            @csrf
+            @method('PUT')
+            <div class="grid grid-cols-1 gap-4">
+                <div>
+                    @include('shared/input', [
+                        'name' => 'tawkto_chat_url',
+                        'label' => __('tawkto::messages.admin.settings.chat_url'),
+                        'value' => setting('tawkto_chat_url'),
+                        'placeholder' => 'https://tawk.to/chat/xxxxxxxxxxxx/xxxxxxxxx',
+                    ])
+                    <p class="text-sm text-gray-500 mt-1">{{ __('tawkto::messages.admin.settings.chat_url_help') }}</p>
+                </div>
+            </div>
+            <button type="submit" class="btn btn-primary">{{ __('global.save') }}</button>
+        </form>
     </div>
 @endsection
