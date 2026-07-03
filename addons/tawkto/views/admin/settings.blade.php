c60112fb87a3cb3df6d9b6b212f7f7b7d175a041
XenoXzOFF <xenoxzperso@gmail.com>
2026-07-03 04:36:35 +0200

    Add margin-top to save button

--- addons/tawkto/views/admin/settings.blade.php
+++ addons/tawkto/views/admin/settings.blade.php
@@ -21,7 +21,7 @@
                     <p class="text-sm text-gray-500 mt-1">{{ __('tawkto::messages.admin.settings.chat_url_help') }}</p>
                 </div>
             </div>
-            <button type="submit" class="btn btn-primary">{{ __('global.save') }}</button>
+            <button type="submit" class="btn btn-primary mt-4">{{ __('global.save') }}</button>
         </form>
     </div>
 @endsection
